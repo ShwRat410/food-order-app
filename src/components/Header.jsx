4b8eb57dbafdee4dda8038332b28f3d9689de983
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-11 14:17:39 +0000

    Using CartContect in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import Button from '../UI/Button'
 import image from '../assets/logo.jpg'
+import {use} from 'react'
+import {CartContext} from '../store/CartContext'
 
 export default function Header(){
+    const cartCtx = use(CartContext)
+    const cartValue = cartCtx.items.reduce((totalnumberofitems,item)=>totalnumberofitems+item.quantity,0)
     return(
         <div id="main-header">
             <div id="title">
@@ -9,7 +13,7 @@ export default function Header(){
                 <h1>FOOD ORDER</h1>
             </div>
             <div>
-                <Button textOnly>Cart(0)</Button>
+                <Button textOnly>Cart({cartValue})</Button>
             </div>
         </div> 
         ) 
