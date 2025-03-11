474122127975749740245d1354d49afe3ccfe5d7
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-11 16:11:32 +0000

    Adding a reusable modal component with useEffect

---
+++ src/UI/Modal.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+
+export default function Modal({children}) {
+
+  const dialog = useRef()
+
+  useEffect(()=>{
+    if(open){
+        dialog.current.showModal()
+    }
+  },[open])
+
+  return (
+    createPortal(()=>{
+        <dialog ref={dialog}>
+            {children}
+        </dialog>
+    },document.getElementById('modal'))
+  )
+}
